615b5d7051ce6ecd02a8eafaf48b1ff78683f958
Fernando Curiel <fcuriel66@mac.com>
2025-08-22 18:23:02 +0200

    Added funtion filter by email

--- filter_users.py
+++ filter_users.py
@@ -10,6 +10,17 @@ def filter_users_by_name(name):
     for user in filtered_users:
         print(user)
 
+
+def filter_users_by_email(email):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if user["email"].lower() == email.lower()]
+
+    for user in filtered_users:
+        print(user)
+
+
 def filter_users_by_age(age):
     with open("users.json", "r") as file:
         users = json.load(file)
@@ -20,13 +31,15 @@ def filter_users_by_age(age):
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? ('name' or 'age'): ").strip().lower()
+    filter_option = input("What would you like to filter by? ('name','email' or 'age'): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
-    # else:
-    #     print("Filtering by that option is not yet supported.")
+
+    elif filter_option == "email":
+        email_to_search = input("Enter an email to filter users: ").strip()
+        filter_users_by_email(email_to_search)
 
     elif filter_option == "age":
         age_to_search = int(input("Enter an age to filter users: "))
